8774fb0098fc940576f354e66b4ebcf53fe609a1
saikoushik619 <Saikoushikguptakonitham@gmail.com>
2026-01-31 22:30:14 +0530

    'fixed_errors'

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.9:8000/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/',
   timeout: 10000,
 });
 
